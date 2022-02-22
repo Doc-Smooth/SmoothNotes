ec22a0048c2a7031a17364229f11a5d566afb7b0
Asger Lindberg <asgerlindberg@yahoo.com>
2022-02-22 13:03:08 +0100

    Add project files.

---
+++ SmoothNotes/SmoothNotes/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using SmoothNotes.Services;
+using SmoothNotes.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmoothNotes
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SmoothNotes/SmoothNotes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SmoothNotes.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SmoothNotes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
